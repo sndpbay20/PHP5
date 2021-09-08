9dc0d131786dd2b83bb71e86111ee86d9a680e95
sndpbay20 <88138178+sndpbay20@users.noreply.github.com>
2021-09-08 18:59:25 +0545

    Add files via upload

---
+++ 14_product_crud/index.php
@@ -0,0 +1,60 @@
+<?php
+$pdo = new PDO('mysql: host= localhost;port=3306;dbname=product_crud', 'root', '');
+$pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$statement-> execute();
+$products = $statement-> fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+var_dump($products);
+echo '</pre>';
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+          integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>product crud</title>
+</head>
+<body>
+<h1>product crud</h1>
+
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
